0fef574a82316634c6170dc532d50fd3d83166f8
amittoshniwal <49736747+charumittalNM@users.noreply.github.com>
2019-04-25 12:50:24 +0530

    use case 2 : get user info implemented

--- src/test/java/test/Twittter.java
+++ src/test/java/test/Twittter.java
@@ -7,27 +7,31 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 import commonFunctions.CommonMethods;
 import commonFunctions.Util;
 
 public class Twittter {
-	@Test
-	public void test() {
-		WebDriver driver = CommonMethods.launchBrowser();
+	WebDriver driver = null;
+	
+	@BeforeSuite
+	public void setup() {
+		driver = CommonMethods.launchBrowser();
 		driver.get(Util.getConfigData("url"));
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	}
+	
+	//@Test(description = "This method will fetch the maximum retry count")
+	public void test() {
 		JavascriptExecutor js = (JavascriptExecutor) driver;
 		int maxRetweet = 0;
 		if (driver.getTitle().equalsIgnoreCase(
 				"STeP-IN Forum (@stepin_forum) | Twitter")) {
-			// twite xpath js-stream-item stream-item stream-item
-			// CommonMethods.scrollPageDown();
 			List<WebElement> tweets = CommonMethods.findElements("xpath",
-			 ".//*[@id='stream-items-id']/li");
-					//".//button[@class='ProfileTweet-actionButton js-actionButton js-actionRetweet']");
+					".//*[@id='stream-items-id']/li");
 			while (tweets.size() < 50) {
 				System.out.println("Total tweets on this page are : "
 						+ tweets.size());
@@ -37,32 +41,45 @@ public class Twittter {
 			}
 			System.out.println("Total tweets on this page are : "
 					+ tweets.size());
-			
 			for (int i = 0; i < tweets.size(); i++) {
 				WebElement element = tweets.get(i);
-				String text = null;
-				int tweetCount = 0 ; 
-				String twitterHandleName = "";
+				int tweetCount = 0;
 				element.getText().indexOf("Retweet");
 				int startindex = element.getText().indexOf("Retweet");
-				//System.out.println(element.getText().indexOf("Retweet")+  7);
 				try {
-					tweetCount = Integer.parseInt(element.getText().substring(startindex + 8, startindex + 9  ).trim());
+					tweetCount = Integer.parseInt(element.getText()
+							.substring(startindex + 8, startindex + 9).trim());
 				} catch (Exception e) {
-					// TODO: handle exception
-					//System.out.println(e.getMessage());
 				}
-				
-				//System.out.println(tweetCount);
-				if(tweetCount > maxRetweet){
-					System.out.println("tweet count is : " + tweetCount);
-					System.out.println("maxRetweet count is : " + maxRetweet);
-					maxRetweet = tweetCount;					
+				if (tweetCount > maxRetweet) {
+					maxRetweet = tweetCount;
 				}
-				// System.out.println(element.getText().equalsIgnoreCase("Retweet"));
-				// div[@Title='Retweet']/following-sibling::span/span
 			}
 		}
 		System.out.println("Max Retweet count is " + maxRetweet);
 	}
+	
+	
+	@Test(description = "This will give users ")
+	public void getUsers(){
+		List<String> userInfo= null; 
+		List<WebElement> users = driver.findElements(By.xpath(".//*[@class='RelatedUsers-users']/div"));
+
+		int counter = 0;
+		if(users.size()>0) {
+			while (counter < 3) {
+				String name = null;
+				String handleName = null;
+				WebElement element = users.get(counter);
+				name = element.findElement(By.tagName("Strong")).getText();
+				handleName = "@" + element.findElement(By.tagName("b")).getText();
+				counter += 1;
+				System.out.println("Name " + name);
+				System.out.println("Handle Name " + handleName);
+			}
+			
+			
+		}
+	}
+	
 }
